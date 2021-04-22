493d0db7f5cb9acedc505531ce474d8f53d74560
piazzesiNiccolo <npiazzesi@gmail.com>
2021-04-22 17:38:08 +0200

    added arg parser

--- run.pl
+++ run.pl
@@ -1,9 +1,41 @@
 :-use_module(library(main)).
-:-use_module(library(option)).
 :-use_module(src/interpret).
 
 
-:- initialization(interpret,main).
+:- initialization(main,main).
+
+main(Argv) :-
+    opt_spec(Spec),
+    opt_parse(Spec, Argv, Opts, _),
+    (
+        member(help(true), Opts) -> show_help
+        ; maplist(format('~w~n'), Opts)
+        
+        
+    ).
+
+show_help:-
+    opt_spec(Spec),
+    opt_help(Spec, HelpText),
+    write('usage: hoftex <options> f\n\n'),
+    write(HelpText).
+
+opt_spec([
+    [opt(help),
+        type(boolean),
+        default(false),
+        shortflags([h]),
+        longflags([help]),
+        help('Show help')],
+    
+    [opt(html),
+        type(boolean),
+        default(false),
+        longflags([html]),
+        help('Convert the output to html')]
+    ]).
+    
+    
 
 
 
--- src/interpret.pl
+++ src/interpret.pl
@@ -1,10 +1,5 @@
 :- module(interpret, [interpret/0]).
 
 interpret :- 
-    set_prolog_flag(toplevel_goal, prolog),
-    current_prolog_flag(argv, Argv),
-    argv_options(Argv, _, Options),
-    option(file(File), Options,"a.tex"),
-    write_ln(Argv),
-    write_ln(File).
+  true.
     
