b7111def7a4dbfc218d3e40d2d3f08012ed53c30
piazzesiNiccolo <npiazzesi@gmail.com>
2021-05-22 15:30:08 +0200

    left associativity should work properly

--- src/hofl.pl
+++ src/hofl.pl
@@ -1,7 +1,7 @@
 :- module(hofl,[parse/2,parse_from_file/2]).
 
 :- use_module(tokenize).
-:- table pre_term//1. 
+:- table pre_term//1,expr//1,aterm//1. 
 /**
 The binary expressions t0 [+,-,*] t1 and function application (t0 t1) rules are left recursive. Using table allows memoization
 of previous results, making the parse predicate terminate as expected (packrat parsing)
@@ -15,32 +15,28 @@ parse_from_file(File,Term) :-
 
 
 pre_term(X) --> ["("],pre_term(X),[")"].
-
-
-pre_term(cond(X,Y,Z)) --> ["if"],pre_term(X),["then"],pre_term(Y),!,["else"],pre_term(Z).
-
+pre_term(cond(X,Y,Z)) --> ["if"],pre_term(X),["then"],pre_term(Y),["else"],pre_term(Z).
 pre_term(rec(var(X),Y)) -->["rec"], pre_term(var(X)),!,["."],pre_term(Y). 
 pre_term(lambda(var(X),Y)) --> ["\\"] , pre_term(var(X)),!,["."],pre_term(Y).
-
-
-pre_term(fst(X)) --> ["fst"], pre_term(X). 
-
-pre_term(snd(X)) --> ["snd"], pre_term(X).
-
-pre_term(tuple(X,Y)) --> ["("],pre_term(X),[","],pre_term(Y),[")"].
-
-
-
-
 pre_term(apply(X,Y)) -->  pre_term(X),["@"], pre_term(Y).
-
-pre_term(mul(X,Y)) --> pre_term(X), ["*"], pre_term(Y).
-
-
-pre_term(bin_op(Op,X,Y)) --> pre_term(X),[Op],{ member(Op,["+","-"])},!, pre_term(Y).
-
-
+pre_term(tuple(X,Y)) --> ["("],pre_term(X),[","],pre_term(Y),[")"].
 pre_term(int(X)) --> [Y],{number_string(X, Y)}.
-
 pre_term(var(X)) --> [Y],{\+number_string(X,Y),atom_string(X, Y),\+member(Y,["\\",".",",","+","-","*","(",")","@"])}.
+pre_term(X) --> expr(X).
+
+expr(X) --> aterm(X).
+expr(bin_op("+",X,Y)) --> expr(X),["+"], aterm(Y).
+expr(bin_op("-",X,Y)) --> expr(X),["-"], aterm(Y).
+
+aterm(mul(X,Y)) --> aterm(X), ["*"], fact(Y).
+aterm(X) --> fact(X),!.
+aterm(fst(X)) --> ["fst"], pre_term(X). 
+aterm(snd(X)) --> ["snd"], pre_term(X).
+
+fact(X) --> ["("], expr(X),[")"].
+fact(X) --> pre_term(X).
+
+
+
+
 
