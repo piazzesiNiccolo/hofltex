82a8e13c9a3872d37d6da108a50eb77a6e551bc8
nicco <npiazzesi@gmail.com>
2021-06-09 22:02:22 +0200

    fixed apply

--- src/hofl.pl
+++ src/hofl.pl
@@ -1,7 +1,7 @@
 :- module(hofl,[parse/2,parse_from_file/2]).
 
 :- use_module(tokenize).
-:- table pre_term//1,expr//1,basic_term//1. 
+:- table pre_term//1,expr//1,basic_term//1,fact//1. 
 /**
 The binary expressions t0 [+,-,*] t1 and function application (t0 t1) rules are left recursive. Using table allows memoization
 of previous results, making the parse predicate terminate as expected (packrat parsing)
@@ -45,15 +45,16 @@ expr(bin_op("-",X,Y)) --> expr(X),["-"], basic_term(Y).
 
 basic_term(mul(X,Y)) --> basic_term(X), ["*"], fact(Y).
 basic_term(X) --> fact(X).
-
-fact(X) --> ["("],expr(X),[")"].
-fact(cond(X,Y,Z)) --> ["if"],pre_term(X),["then"],pre_term(Y),["else"],pre_term(Z).
+fact(X) --> ["("],pre_term(X),[")"].
+fact(apply(X,Y)) -->  pre_term(X),["@"], pre_term(Y).
+fact(cond(X,Y,Z))--> ["if"],pre_term(X),["then"],pre_term(Y),["else"],pre_term(Z).
 fact(tuple(X,Y)) --> ["("],pre_term(X),[","],pre_term(Y),[")"].
 fact(fst(X)) --> ["fst"], pre_term(X). 
 fact(snd(X)) --> ["snd"], pre_term(X).
-fact(apply(X,Y)) -->  pre_term(X),["@"],!, pre_term(Y).
+
 
 fact(int(X)) --> [Y],{number_string(X, Y)}.
+
 fact(id(X)) --> [Y],{\+number_string(X,Y),atom_string(X, Y),\+member(Y,["\\",".",",","+","-","*","(",")","@"])}.
 
 
